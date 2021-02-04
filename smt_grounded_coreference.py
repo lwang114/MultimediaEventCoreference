866cfc08bea7e8ad702eb3feead4037f7f9d13e5
lwang114 <lwang114@illinois.edu>
2021-02-03 20:45:54 -0600

    fixed bugs in smt coref

--- smt_grounded_coreference.py
+++ smt_grounded_coreference.py
@@ -1,8 +1,12 @@
+import nltk
 import numpy as np
 import os
 import json
 import logging
+import torch
 from itertools import combinations
+from allennlp.predictors.predictor import Predictor
+import allennlp_models.structured_prediction
 from evaluator import Evaluation
 from conll import write_output_file
 
@@ -35,14 +39,16 @@ class SMTCoreferencer:
     self.out_path = out_path
     if not os.path.exists(self.out_path):
       os.makedirs(self.out_path)
-
+    self.dep_parser = Predictor.from_path('https://storage.googleapis.com/allennlp-public-models/biaffine-dependency-parser-ptb-2020.04.06.tar.gz')
+      
     self.mentions,\
     self.tokens_ids,\
     self.cluster_ids,\
     self.vocabs = self.load_corpus(doc_path, mention_path)
     self.mention_probs = 1./len(self.vocabs) * np.ones((len(self.vocabs), len(self.vocabs)))
 
-  def load_corpus(self, doc_path, mention_path): # TODO Find head words of each mention
+    
+  def load_corpus(self, doc_path, mention_path):
     '''
     :returns doc_ids: list of str
     :returns mentions: list of list of str
@@ -55,10 +61,12 @@ class SMTCoreferencer:
     mention_list = json.load(open(mention_path))
     mention_dict = {} 
     mentions = []
+    mention_heads = []
     spans = []
     cluster_ids = []
     vocabs = {'###UNK###': 0}
-
+    dep_rels = set()
+    
     for m in mention_list:
       # Create a mapping from span to a tuple of (token, cluster_id) 
       tokens_ids = m['tokens_ids']
@@ -70,46 +78,70 @@ class SMTCoreferencer:
         
     for doc_id in sorted(documents): # XXX
       mentions.append([])
+      mention_heads.append([])
       spans.append([])
       cluster_ids.append([])
-
+      
       tokens = [t[2] for t in documents[doc_id]]
-      for span in sorted(mention_dict[doc_id]):
+      postags = [t[1] for t in nltk.pos_tag(tokens)]
+      instance = self.dep_parser._dataset_reader.text_to_instance(tokens, postags)
+      '''
+      parse = self.dep_parser.predict_instance(instance)
+      dep_head = parse['predicted_heads']
+      dep_rel = parse['predicted_dependencies']
+      dep_rels.update(dep_rel)
+      '''
+      for span in sorted(mention_dict[doc_id]): # TODO Find head words of each mention
         cluster_id = mention_dict[doc_id][span]
-        mention_token = tokens[span[0]:span[1]+1]
-        for t in mention_token:
-          if not t in vocabs:
-            vocabs[t] = len(vocabs)
+        mention_token = tokens[span[0]:span[1]]
+
+        '''
+        if span[0] != span[1]:
+          mention_heads[-1].append([])
+          for x_idx, x in enumerate(range(span[0], span[1]+1)):
+            if dep_head[x] - 1 < span[0] or dep_head[x] - 1 > span[1]:
+              print('Head for mention {} at [{}, {}] is {} with dep head = {}'.format(' '.join(mention_token), span[0], span[1], tokens[x], dep_head[x] - 1))
+              mention_heads[-1][-1].append(x_idx)
+        else:
+          mention_heads[-1].append([0])
+        '''
+        if len(mention_token) == 0:
+          mention_token = ['###UNK###']
+        if not mention_token[-1] in vocabs:
+          vocabs[mention_token[-1]] = len(vocabs)
         mentions[-1].append(mention_token)
         spans[-1].append(span)
         cluster_ids[-1].append(cluster_id)
     
     logger.info('Number of documents = {}'.format(len(mentions)))
+    logger.info('Vocab size = {}'.format(len(vocabs)))
+    logger.info('Dependency relations = {}'.format(dep_rels))
     return mentions, spans, cluster_ids, vocabs
 
 
   def fit(self, n_epochs=10):
     for epoch in range(n_epochs):
       N_c = [np.zeros((len(sent), len(sent))) for sent in self.mentions]
-      N_m = np.zeros((len(self.vocabs), len(self.vocabs)))
+      N_m = EPS * np.ones((len(self.vocabs), len(self.vocabs)))
 
       # Compute counts
       for sent_idx, sent in enumerate(self.mentions):
-        for first_idx in range(len(sent)):
-          for second_idx in range(first_idx + 1):
+        for second_idx in range(len(sent)):
+          for first_idx in range(second_idx):
             first = self.vocabs[sent[first_idx][-1]]
             second = self.vocabs[sent[second_idx][-1]]
             N_c[sent_idx][first_idx, second_idx] += self.mention_probs[first][second]
+
         N_c[sent_idx] /= N_c[sent_idx].sum(axis=0) + EPS
         
-        for first_idx in range(len(sent)):
-          for second_idx in range(first_idx + 1):
+        for second_idx in range(len(sent)):
+          for first_idx in range(second_idx):
             first = self.vocabs[sent[first_idx][-1]]
             second = self.vocabs[sent[second_idx][-1]]
             N_m[first, second] += N_c[sent_idx][first_idx, second_idx]
 
       # Update mention probs     
-      self.mention_probs = N_m / (N_m.sum(axis=-1, keepdims=True) + EPS)
+      self.mention_probs = N_m / N_m.sum(axis=-1, keepdims=True)
 
       # Log stats
       logger.info('Epoch {}, log likelihood = {:.3f}'.format(epoch, self.log_likelihood()))
@@ -128,7 +160,7 @@ class SMTCoreferencer:
       
       for m_idx, second in enumerate(mentions):
         p = 0.
-        for _, first in enumerate(mentions[:m_idx+1]):
+        for _, first in enumerate(mentions[:m_idx]):
           first_idx = self.vocabs.get(first[-1], 0) # TODO Use head word
           second_idx = self.vocabs.get(second[-1], 0)
           p += self.mention_probs[first_idx][second_idx] 
@@ -196,8 +228,8 @@ class SMTCoreferencer:
                         out_path,
                         doc_id)
     
-    preds = np.asarray(preds)
-    golds = np.asarray(golds)
+    preds = torch.from_numpy(np.asarray(preds))
+    golds = torch.from_numpy(np.asarray(golds))
     pairwise_eval = Evaluation(preds, golds) 
     
     precision = pairwise_eval.get_precision()
@@ -207,11 +239,12 @@ class SMTCoreferencer:
     return f1 
 
 if __name__ == '__main__':
-  doc_path_train = 'data/video_m2e2/mentions/train.json'
-  mention_path_train = 'data/video_m2e2/mentions/train_mixed.json'
-  doc_path_test = 'data/video_m2e2/mentions/test.json'
-  mention_path_test = 'data/video_m2e2/mentions/test_mixed.json'
-  out_path = 'exp/smt/pred_mixed'
+  dataset = 'ecb'
+  doc_path_train = 'data/{}/mentions/train.json'.format(dataset)
+  mention_path_train = 'data/{}/mentions/train_mixed.json'.format(dataset)
+  doc_path_test = 'data/{}/mentions/test.json'.format(dataset)
+  mention_path_test = 'data/{}/mentions/test_mixed.json'.format(dataset)
+  out_path = 'models/smt_{}/pred_mixed'.format(dataset)
 
   model = SMTCoreferencer(doc_path_train, mention_path_train, out_path=out_path+'_train')
   model.fit()
