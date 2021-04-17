18aa66c158c79b6d910908fedf32bc920f9c64d8
Liming Wang <lwang114@ifp-10.ifp.uiuc.edu>
2021-04-17 13:38:11 -0500

    hdp event aligner

--- dnnhmmdnn/hdp_event_aligner.py
+++ dnnhmmdnn/hdp_event_aligner.py
@@ -2,10 +2,10 @@ import math
 import random
 import numpy as np
 from copy import deepcopy
+import numpy as np
+import logging
 import json
 import os
-<<<<<<< HEAD
-=======
 import pyhocon
 import itertools
 import codecs
@@ -14,10 +14,10 @@ import argparse
 import nltk
 from nltk.stem import WordNetLemmatizer
 from evaluator import Evaluation, CoNLLEvaluation
->>>>>>> c2416be1930cb2a0b593ce3919036488392a9268
 random.seed(2)
 # Part of the code modified from vpyp: https://github.com/vchahun/vpyp/blob/master/vpyp/pyp.py
 
+# logger = logging.getLogger(__name__)
 class Restaurant:
   # Attributes:
   # ----------
@@ -74,11 +74,7 @@ class Restaurant:
       else:
         w = self.alpha0
     else:
-<<<<<<< HEAD
-      w = alpha0
-=======
       w = self.alpha0 * self.p_init 
->>>>>>> c2416be1930cb2a0b593ce3919036488392a9268
     
     if k in self.name2table:
       i = self.name2table[k]
@@ -86,35 +82,28 @@ class Restaurant:
     return w / (self.alpha0 + self.ncustomers) 
 
   def log_likelihood(self):
-<<<<<<< HEAD
-    ll = math.lgamma(self.alpha0) - math.lgamma(self.alpha0 + self.ncustomers)
-    ll += sum(math.lgamma(self.tables[i] + self.alpha0 * self.p_init[k]) for i, k in enumerate(self.table_names))
-    ll += sum(self.p_init[k] - math.lgamma(self.alpha0 * self.p_init[k]) for k in self.table_names)
-=======
     if self.p_init == 0:
       ll = math.lgamma(self.alpha0) - math.lgamma(self.alpha0 + self.ncustomers)\
               + sum(math.lgamma(self.tables[i]) for i in range(self.ntables))\
               + self.ntables * math.log(self.alpha0)
     else:
-      ll = math.lgamma(self.alpha0) - math.lgamma(self.alpha0 + self.ncustomers)\
+      ll = math.lgamma(self.alpha0 * self.p_init * (self.ntables + 1)) - math.lgamma(self.alpha0 * self.p_init * (self.ntables + 1) + self.ncustomers)\
               + sum(math.lgamma(self.tables[i] + self.alpha0 * self.p_init) for i in range(self.ntables))\
               - self.ntables * math.lgamma(self.alpha0 * self.p_init)\
               + self.ntables * math.log(self.alpha0 * self.p_init)
->>>>>>> c2416be1930cb2a0b593ce3919036488392a9268
     return ll
 
   def save(self, outputDir='./', returnStr=False):
-    with open(outputDir + 'tables.txt', 'w') as f:
-      sorted_indices = sorted(list(range(self.ntables)), key=lambda x:self.tables[x], reverse=True)
-      outStr = ''
-      for i in sorted_indices[:10]:
-        outStr += '%s %d\n' % (self.table_names[i], self.tables[i])
+    sorted_indices = sorted(list(range(self.ntables)), key=lambda x:self.tables[x], reverse=True)
+    outStr = ''
+    for i in sorted_indices[:10]:
+      outStr += 'Table %d: %s %d\n' % (i, self.table_names[i], self.tables[i])
 
     if returnStr:
       return outStr
     else:
-      f.write(outStr)
-    
+      with open(outputDir + 'tables.txt', 'w') as f:
+        f.write(outStr)
 
 class HDPEventAligner(object):
   def __init__(self,
@@ -131,25 +120,16 @@ class HDPEventAligner(object):
         a_feats_train: a list of list of list of strs,
         cluster_ids: a list of list of ints
     """ 
-    self.e_feats_train = e_feats_train
-    self.a_feats_train = a_feats_train
+    self.e_feats_train = event_features_train
+    self.a_feats_train = entity_features_train
     self.alpha0 = alpha0
     self.p_init_event = p_init_event
     self.p_init_entity = p_init_entity
     self.event_crp = Restaurant(alpha0) # Keep track of counts for each event
-    self.feature_crps = [] # Keep track of counts for each feature 
-                                                                   # within each event
+    self.feature_crps = [] # Keep track of counts for each feature within each event
     self.cluster_ids = [[] for _ in self.e_feats_train]
 
   def prob(self, c, e, a):
-<<<<<<< HEAD
-    p_c = self.event_crp.prob(c)
-    
-    p_e = self.feature_crps[c][0].prob(e) if c < len(self.feature_crps) else 1.
-    p_a = np.prod([self.feature_crps[c][1].prob(a_i) for a_i in a])\
-              if c < len(self.feature_crps) else 1.
-    return p_c * p_e * p_a
-=======
     p = self.event_crp.prob(c)
     if c in self.event_crp.name2table:
       table_idx = self.event_crp.name2table[c]
@@ -158,19 +138,15 @@ class HDPEventAligner(object):
     else:
       p *= self.p_init_event * (self.p_init_entity ** len(a))
     return p
->>>>>>> c2416be1930cb2a0b593ce3919036488392a9268
   
   def log_likelihood(self):
     ll = self.event_crp.log_likelihood()
-    for c in range(self.event_crp.ntables):
+    for table_idx in range(self.event_crp.ntables):
       for i in range(2):
-        ll += self.feature_crps[c][i].log_likelihood()
+        ll += self.feature_crps[table_idx][i].log_likelihood()
     return ll
 
   def gibbs_sample(self, e, a):
-<<<<<<< HEAD
-    P = [prob(c, e, a) for c in range(self.event_crp.ntables+1)]
-=======
     P = [self.prob(c, e, a) for c in self.event_crp.table_names]
     P.append(self.prob(-1, e, a))
     '''
@@ -184,65 +160,57 @@ class HDPEventAligner(object):
     print('e, a: ', e, a)
     '''
 
->>>>>>> c2416be1930cb2a0b593ce3919036488392a9268
     norm = sum(P)
     x = norm * random.random()
-    for c, w in enumerate(P):
-      if x < w:
-        break
+    for c, w in zip(self.event_crp.table_names+[-1], P):
+      if x < w: return c
       x -= w
-    return c
+    return -1
 
-  def train(self, n_iter=100, out_dir='./'):
+  def train(self, n_iter=35, out_dir='./'):
     order = list(range(len(self.e_feats_train)))
     for i_iter in range(n_iter):
+      random.shuffle(order)
       for i in order:
         e_feat = self.e_feats_train[i]
         a_feat = self.a_feats_train[i]
+        mention_order = list(range(len(e_feat)))
+        random.shuffle(mention_order) 
         if i_iter > 0:
-          for c, e, a in zip(self.cluster_ids[i], e_feat, a_feat):
-            self.event_crp.unseat_from(c)
-            self.feature_crps[c][0].unseat_from(e)
-            for a_i in a:
-              self.feature_crps[c][1].unseat_from(a_i) 
-            
-            if self.feature_crps[c][0].ntables == 0:
-              del self.feature_crps[c]
+          for i_m in mention_order:
+            c = self.cluster_ids[i][i_m]
+            e = e_feat[i_m] 
+            a = a_feat[i_m]
+            self.unseat_from(c, e, a)
         assert len(self.feature_crps) == self.event_crp.ntables 
 
-        new_cluster_ids = [self.gibbs_sample(e, a) for e, a in zip(e_feat, a_feat)]
+        new_cluster_ids = [-1]*len(e_feat)
+        for i_m in mention_order:
+          e, a = e_feat[i_m], a_feat[i_m]
+          c = self.gibbs_sample(e, a)
+          if c == -1:
+            new_c = 0
+            while new_c in self.event_crp.name2table: # Create a new key for the new event
+              new_c += 1
+            c = new_c
+          self.seat_to(c, e, a)
+          new_cluster_ids[i_m] = c
         self.cluster_ids[i] = deepcopy(new_cluster_ids)
-        for c, e, a in zip(new_cluster_ids, e_feat, a_feat):
-          if c == self.event_crp.ntables:
-            self.event_crp.seat_to(c)
-            # Create CRPs for a new event
-            new_event_crp = Restaurant(self.alpha0)
-            new_argument_crp = Restaurant(self.alpha0)
-            new_event_crp.seat_to(e)
-            for a_i in a:
-              new_argument_crp.seat_to(a_i)
-            self.feature_crps.append([new_event_crp, new_argument_crp])
-          else:
-            self.event_crp.seat_to(c)
-            self.feature_crps[c][0].seat_to(e)
-            for a_i in a:
-              self.feature_crps[c][1].seat_to(a_i)
-
+          
       if i_iter % 10 == 0:
         self.save(out_dir)
-      print(f'Iteration {i_iter}, log likelihood = {self.log_likelihood():d}')
+      print(f'Iteration {i_iter}, log likelihood = {self.log_likelihood():.1f}')
 
   def cluster(self, 
               event_features_test,
               entity_features_test):
     cluster_ids = []
     for e_feat, a_feat in zip(event_features_test, entity_features_test):
-      cluster_ids.append([np.argmax([self.prob(c, e, a) for c in range(self.event_crp.ntables)])\
-                                                            for e, a in zip(e_feat, a_feat)])
+      table_idxs = [np.argmax([self.prob(c, e, a) for c in self.event_crp.table_names])\
+                                                            for e, a in zip(e_feat, a_feat)]
+      cluster_ids.append(np.asarray([self.event_crp.table_names[table_idx] for table_idx in table_idxs]))
     return cluster_ids
 
-<<<<<<< HEAD
-=======
   def seat_to(self, c, e, a):
     if not c in self.event_crp.name2table: # Create CRPs for a new event
       self.event_crp.seat_to(c)
@@ -271,24 +239,15 @@ class HDPEventAligner(object):
       del self.feature_crps[-1]
       assert self.event_crp.ntables == len(self.feature_crps)
       
->>>>>>> c2416be1930cb2a0b593ce3919036488392a9268
   def save(self, out_dir='./'):
     self.event_crp.save(os.path.join(out_dir, 'event_crp_'))
+    feat_names = ['triggers', 'arguments']
     out_str = ''
-    f_out = open(os.path.join(out_dir, 'feature_crps_'))
-    for c in range(self.event_crp.ntables):
-      feat_names = ['triggers', 'arguments']
+    f_out = open(os.path.join(out_dir, 'feature_crps_tables.txt'), 'w')
+    for c in self.event_crp.table_names:
+      table_idx = self.event_crp.name2table[c]
       for i in range(2):
         out_str += f'{feat_names[i]}\n'
-<<<<<<< HEAD
-        out_str += self.feature_crps[c][i].save(returnStr=True)
-        out_str += '\n'
-    f_out.write(out_str)
-    f_out.close()
-
-def load_text_features(config, split): # TODO
-  lemmatizer = WordNetLemmatizer() 
-=======
         out_str += self.feature_crps[table_idx][i].save(returnStr=True)
       out_str += '\n'
     f_out.write(out_str)
@@ -318,7 +277,6 @@ def to_antecedents(labels):
     
 def load_text_features(config, split):
   lemmatizer = WordNetLemmatizer()
->>>>>>> c2416be1930cb2a0b593ce3919036488392a9268
   event_mentions = json.load(codecs.open(os.path.join(config['data_folder'], f'{split}_events.json'), 'r', 'utf-8'))
   doc_train = json.load(codecs.open(os.path.join(config['data_folder'], f'{split}.json')))
 
@@ -332,8 +290,7 @@ def load_text_features(config, split):
   tokens_all = [] 
 
   for m in event_mentions:
-    if m['doc_id'] in doc_to_feat:
-      if not m['doc_id'] in label_dict:
+      if not m['doc_id'] in label_dicts:
         label_dicts[m['doc_id']] = {}
       token = lemmatizer.lemmatize(m['tokens'].lower(), pos='v')
       span = (min(m['tokens_ids']), max(m['tokens_ids']))
@@ -349,8 +306,7 @@ def load_text_features(config, split):
           a_token = lemmatizer.lemmatize(a['tokens'].lower())
           label_dicts[m['doc_id']][span]['arguments'][(min(a['tokens_ids']), max(a['tokens_ids']))] = a_token
         
-  for feat_idx, doc_id in enumerate(sorted(label_dicts)): # XXX
-    feat_id = doc_to_feat[doc_id]
+  for feat_idx, doc_id in enumerate(sorted(label_dicts)[:20]): # XXX
     label_dict = label_dicts[doc_id]
     spans = sorted(label_dict)
     a_spans = [[a_span for a_span in sorted(label_dict[span]['arguments'])] for span in spans]
@@ -358,23 +314,23 @@ def load_text_features(config, split):
     entities = [[label_dict[span]['arguments'][a_span] for a_span in sorted(label_dict[span]['arguments'])] for span in spans]
     cluster_ids = [label_dict[span]['cluster_id'] for span in spans]
     
-    event_feats_all.append(events)
-    entity_feats_all.append(entities)
+    event_feats.append(events)
+    entity_feats.append(entities)
     doc_ids.append(doc_id)
     spans_all.append(spans)
     spans_entity_all.append(a_spans)    
     cluster_ids_all.append(np.asarray(cluster_ids))
     tokens_all.append([t[2] for t in doc_train[doc_id]])
-  return event_feats_all, 
-         entity_feats_all,
-         ea_maps_all
-         doc_ids_all, 
-         spans_all, 
-         spans_entity_all,
-         cluster_ids_all,
-         tokens_all
+  return event_feats,\
+    entity_feats,\
+    doc_ids,\
+    spans_all,\
+    spans_entity_all,\
+    cluster_ids_all,\
+    tokens_all
 
-def load_data(config): # TODO
+def load_data(config):
+  lemmatizer = WordNetLemmatizer() 
   event_mentions_train = json.load(codecs.open(os.path.join(config['data_folder'], 'train_events.json'), 'r', 'utf-8'))
   doc_train = json.load(codecs.open(os.path.join(config['data_folder'], 'train.json')))
   event_mentions_test = json.load(codecs.open(os.path.join(config['data_folder'], 'test_events.json'), 'r', 'utf-8'))
@@ -382,7 +338,7 @@ def load_data(config): # TODO
 
   vocab = dict()
   vocab_freq = dict()
-  vocab_entity = dict{}
+  vocab_entity = dict()
   vocab_entity_freq = dict()
   for m in event_mentions_train + event_mentions_test:
     trigger = m['tokens']
@@ -393,18 +349,13 @@ def load_data(config): # TODO
     else:
       vocab_freq[trigger] += 1
     
-<<<<<<< HEAD
-    for a in trigger['arguments']:
-      argument = a['text']
-=======
     for a in m['arguments']:
       if 'text' in a:
         argument = a['text']
       else:
         argument = a['tokens']
->>>>>>> c2416be1930cb2a0b593ce3919036488392a9268
       argument = lemmatizer.lemmatize(argument.lower())
-      if not argument in vocab:
+      if not argument in vocab_entity:
         vocab_entity[argument] = len(vocab_entity)
         vocab_entity_freq[argument] = len(vocab_entity)
       else:
@@ -413,9 +364,9 @@ def load_data(config): # TODO
   json.dump(vocab_freq, open('vocab_freq.json', 'w'), indent=2)
   json.dump(vocab_entity_freq, open('vocab_entity_freq.json', 'w'), indent=2)
 
+  vocab_size = len(vocab)
+  vocab_entity_size = len(vocab_entity)
   print(f'Vocab size: {vocab_size}, vocab entity size: {vocab_entity_size}')
-  print(f'Number of training examples: {len(label_dict_train)}')
-  print(f'Number of test examples: {len(label_dict_test)}')
 
   event_feats_train,\
   entity_feats_train,\
@@ -424,7 +375,8 @@ def load_data(config): # TODO
   spans_entity_train,\
   cluster_ids_train,\
   tokens_train = load_text_features(config, split='train')
-
+  print(f'Number of training examples: {len(event_feats_train)}')
+  
   event_feats_test,\
   entity_feats_test,\
   doc_ids_test,\
@@ -432,7 +384,8 @@ def load_data(config): # TODO
   spans_entity_test,\
   cluster_ids_test,\
   tokens_test = load_text_features(config, split='test')
-
+  print(f'Number of test examples: {len(event_feats_test)}')
+  
   return event_feats_train,\
          entity_feats_train,\
          doc_ids_train,\
@@ -446,7 +399,9 @@ def load_data(config): # TODO
          spans_test,\
          spans_entity_test,\
          cluster_ids_test,\
-         tokens_test
+         tokens_test,\
+         vocab,\
+         vocab_entity
  
 if __name__ == '__main__':
   parser = argparse.ArgumentParser() # formatter=argparse.DefaultsHelpArgumentFormatter)
@@ -457,7 +412,7 @@ if __name__ == '__main__':
   config = pyhocon.ConfigFactory.parse_file(config_file)
   if not os.path.isdir(config['model_path']):
     os.makedirs(config['model_path'])
-  logging.basicConfig(filename=os.path.join(config['model_path'], 'train.log'))
+  logging.basicConfig(filename=os.path.join(config['model_path'], 'train.log'), level=logging.DEBUG)
 
   event_feats_train,\
   entity_feats_train,\
@@ -472,22 +427,18 @@ if __name__ == '__main__':
   spans_test,\
   spans_entity_test,\
   cluster_ids_test,\
-  tokens_test = load_data(config)
+  tokens_test,\
+  vocab, vocab_entity = load_data(config)
   Ke = len(vocab)
   Ka = len(vocab_entity)
 
   ## Model training
-  aligner = HDPEventAligner(event_feats_train, 
-                            entity_feats_train,
-<<<<<<< HEAD
-                            alpha0=1.)
-  aligner.train(100, out_dir=config['model_path'])
-=======
-                            alpha0=1.,
+  aligner = HDPEventAligner(event_feats_train+event_feats_test, 
+                            entity_feats_train+entity_feats_test,
+                            alpha0=config['alpha0'],
                             p_init_event=1./5,
                             p_init_entity=1./5)
   aligner.train(35, out_dir=config['model_path'])
->>>>>>> c2416be1930cb2a0b593ce3919036488392a9268
 
   ## Test and evaluation
   pred_cluster_ids = aligner.cluster(event_feats_test, entity_feats_test)
@@ -499,7 +450,7 @@ if __name__ == '__main__':
   # Compute pairwise scores
   pairwise_eval = Evaluation(pred_labels, gold_labels)  
   print(f'Pairwise - Precision: {pairwise_eval.get_precision()}, Recall: {pairwise_eval.get_recall()}, F1: {pairwise_eval.get_f1()}')
-  logger.info(f'Pairwise precision: {pairwise_eval.get_precision()}, recall: {pairwise_eval.get_recall()}, F1: {pairwise_eval.get_f1()}')
+  logging.info(f'Pairwise precision: {pairwise_eval.get_precision()}, recall: {pairwise_eval.get_recall()}, F1: {pairwise_eval.get_f1()}')
   
   # Compute CoNLL scores and save readable predictions
   conll_eval = CoNLLEvaluation()
@@ -509,10 +460,21 @@ if __name__ == '__main__':
     pred_clusters, gold_clusters = conll_eval(torch.LongTensor(span),
                                               torch.LongTensor(antecedent),
                                               torch.LongTensor(span),
-                                              torch.LongTensor(cluster_id)) 
+                                              torch.LongTensor(gold_cluster_id)) 
     pred_clusters_str, gold_clusters_str = conll_eval.make_output_readable(pred_clusters, gold_clusters, token) 
     token_str = ' '.join(token)
     f_out.write(f'{doc_id}: {token_str}\n')
     f_out.write(f'Pred: {pred_clusters_str}\n')
     f_out.write(f'Gold: {gold_clusters_str}\n\n')
   f_out.close() 
+
+  muc, b_cubed, ceafe, avg = conll_eval.get_metrics()
+  conll_metrics = muc+b_cubed+ceafe+avg
+  print('MUC - Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}, '
+        'Bcubed - Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}, '
+        'CEAFe - Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}, '
+        'CoNLL - Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}'.format(*conll_metrics)) 
+  logging.info('MUC - Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}, '
+              'Bcubed - Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}, '
+              'CEAFe - Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}, '
+              'CoNLL - Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}'.format(*conll_metrics))
